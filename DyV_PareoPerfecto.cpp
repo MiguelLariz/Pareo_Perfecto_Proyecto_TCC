1b1f3ef22ca8992eb19b9f804508ece312e9aed2
Miguel Lariz <duronlarizm@gmail.com>
2024-05-20 09:40:25 -0600

    Funciones de lectura de matriz actualizada

--- DyV_PareoPerfecto.cpp
+++ DyV_PareoPerfecto.cpp
@@ -22,6 +22,7 @@
 #include <iostream>
 #include <math.h>
 #include <cmath>
+#include <cstdlib>
 #include <fstream>
 #include <string>
 #include <ctime>
@@ -45,6 +46,12 @@ void copiarMatriz( ); // Copiar de la matriz a la matrizClon
 void ordenVertices( ); // Ordenar los vertices del menor conexiones al mayor
 void pareo( ); // Pareo de vertices
 
+// LLenado de valores de matriz
+void matrizAleatoria( ); // Se llena con valores aleatorios
+void leerArchivo( ); // Se lee un archivo txt para llenar la matriz
+void valoresManual( ); // Llenar la matriz con valores manualmente
+bool matrizCorrecta( ); // Revisar si esta correcta la matriz
+
 
 // Funcion principal
 int main()
@@ -53,6 +60,207 @@ int main()
 	system( "title Algoritmo Gloton" ); // Cambiar el titulo de la consola
 	setlocale( LC_ALL , "" ); // Aceptar acentos
 
+    bool terminar = true , error = false;
+    char salir;
+    char opcion;
+
+    while( terminar )
+    {
+
+        // Reiniciar la variable si hay algun error
+        error = false;
+
+        do
+        {
+
+            // Limpiar consola
+            system( "cls" );
+
+            // Opcion diferente a las mostradas
+            if ( error ) cout << " Opcion incorrecta. ";
+
+            // Titulo
+            cout << " Lectura de matriz: " << endl;
+
+            // Opciones
+            cout << " 1) Matriz aleatoria. " << endl;
+            cout << " 2) Lectura de archivo de texto. " << endl;
+            cout << " 3) Ingresar valores manualmente. " << endl;
+
+            // Lectura de opcion
+            cout << "Opcion: "; cin >> opcion;
+
+            // Por si hay algun error
+            error = true;
+
+        // Valir la opcion
+        } while ( opcion != '1' && opcion != '2' && opcion != '3' );
+
+        vaciarMatriz( );
+
+        switch ( opcion )
+        {
+            // Opcion 1 para llenado de matriz aleatoria
+            case '1': matrizAleatoria( ); break;
+
+            // Opcion 2 para leer un archivo de texto txt
+            case '2': leerArchivo( ); break;
+
+            // Opcion 3 para llenar valores manualmente
+            case '3': valoresManual( ); break;
+            
+            // Opcion por defecto (nunca se hace)
+            default: break;
+        }
+
+        if ( matrizCorrecta( ) )
+        {
+
+            // Iniciar con el algoritmo
+            cout << "Matriz aceptada" << endl;
+
+            // Acomodar los vertices para que se hagan en el orden correcto
+            // del que tenga menro numero de aristas al que tenga mas
+            ordenVertices( );
+
+            // Separador
+            cout << endl << endl;
+
+            system( "pause" );
+
+            do
+            {
+                system( "cls" );
+                cout << "Quieres terminar el algoritmo? (y/n) "; cin >> salir;
+            } while ( salir != 'y' && salir != 'n' );
+
+            // Si terminarlo
+            if ( salir == 'y') terminar = false;
+
+            // No terminarlo y reiniciar el algoritmo
+            if ( salir == 'n' ) cout << "Reiniciando..." << endl << endl , system( "pause" );
+
+        }
+        else
+        {
+
+            system( "cls" ); // Limpiar consola
+
+            // Matriz con valores erroneos
+            cout << " Matriz con valores incorrectos " << endl;
+
+            // Posibles errones
+            cout << " Posibles erores: Algún vertice no tiene aristas o tiene peso negativo " << endl;
+
+            // Esperar una tecla
+            system( "pause" );
+
+            // Seguir con las iteraciones del ciclo
+            continue;
+        }
+
+    }
+
+    system( "cls" ); // Limpiar consola
+    cout << "Saliendo...";
+
+    cout << endl << endl; // Separador
+    system( "pause" ); // Hacer que pare antes de terminar
+	return 0; // Terminar el algoritmo
+}
+
+
+// Matriz llenada aleatoriamente
+void matrizAleatoria( )
+{
+
+    cout << "Matriz aleatoria" << endl;
+
+    // Por si hay un error
+    bool valorCorrecto = false;
+
+    // Si el renglon generado es todo ceros
+    bool todoCeros = true;
+
+    // Variable para agregar ceros a la matriz
+    int randomCero = 0;
+
+    // Cambiar peso de arista
+    int cambioPeso = 0;
+
+    do
+    {
+        system( "cls" ); // Limpiar consola
+
+        // Tamanio menor a 0 o mayor a 100
+        if ( valorCorrecto ) cout << " El valor ingresado es incorrecto, intenta con otro " << endl;
+
+        // Lectura del tamanio de la matriz
+        cout << "Tamanio de la matriz:"; cin >> tamanioMatriz;
+
+        // Error en el tamanio
+        valorCorrecto = true;
+
+    // Valir la lectura
+    } while ( tamanioMatriz <= 0 || tamanioMatriz >= 101 || ( tamanioMatriz % 2 ) != 0 );
+
+
+    // Generacion de numeros aleatorios
+    for ( int i = 0; i < tamanioMatriz; i++ )
+    {
+        for ( int k = ( i + 1 ); k < tamanioMatriz; k++ )
+        {
+            // Generar random para aumentar el tamanio de los 0
+            randomCero = rand( ) % 100;
+
+            // Generar valores entre 0 y 10
+            if ( randomCero <= 50 ) matriz[ i ][ k ] = 0;
+            else matriz[ i ][ k ] = 1 + rand() % 10;
+        }
+
+        // Reiniciar variables por si todo el renglon es 0
+        todoCeros = true;
+
+        // Recorrec todo el renglon
+        for ( int m = ( i + 1 ); m < tamanioMatriz; m++ )
+        {
+            if ( matriz[ i ][ m ] != 0 ) todoCeros = false;
+        }
+
+        // Si es la ultima posicion
+        if ( ( i + 1 ) == tamanioMatriz ) todoCeros = false;
+
+        // Cambiar algun peso por 1 si es todo 0
+        if ( todoCeros && ( i + 1 ) != tamanioMatriz )
+        {
+            cambioPeso = i + rand() % ( tamanioMatriz - i );
+            matriz[ i ][ cambioPeso ] = 1;
+        }
+    }
+
+    // Triangular inferior
+    for ( int i = 0; i < tamanioMatriz; i++ )
+    {
+
+        // Hacer espejo de la matriz
+        for ( int k = ( i + 1 ); k < tamanioMatriz; k++ )
+        {
+            matriz[ k ][ i ] = matriz[ i ][ k ];
+        }
+    }
+
+    // Mostrar la matriz generada
+    cout << " Matriz generada" << endl;
+    imprimirMatriz( );
+
+    // Detener
+    system( "pause" );
+}
+
+// Debe existir un archivo en la misma carpeta que el codigo para poder funcionar
+// el archivo se debe llamar "matriz_adyacencia.txt"
+void leerArchivo( )
+{
     vaciarMatriz( ); // Vaciar completamente la matriz global
 
     string nombreArchivo = "matriz_adyacencia.txt"; // Encontrar el archivo
@@ -76,19 +284,83 @@ int main()
             }
 
         }
-            
+
+        // Tamanio de la matriz, se le suma 1 porque el ciclo for empieza en el 0
         tamanioMatriz = k + 1;
         indice = 0;
     }
 
+    // Titulo de la lectura
     cout << endl << "Matriz obtenida del documento 'matriz_adyacencia.txt' " << endl << endl;
     imprimirMatriz( ); // Mostrar la matriz obtenida del archivo
+}
 
-    ordenVertices( );
 
-    cout << endl << endl; // Separador
-    system( "pause" ); // Hacer que pare antes de terminar
-	return 0; // Terminar el algoritmo
+// Llenar la matriz de manera manual
+void valoresManual( )
+{
+    bool valorCorrecto = false;
+
+    do
+    {
+        system( "cls" ); // Limpiar consola
+
+        // Valores menor a 0 o mayor a 100
+        if ( valorCorrecto ) cout << " El valor ingresado es incorrecto, intenta con otro " << endl;
+
+        // Lectura del tamanio de la matriz
+        cout << "Tamanio de la matriz:"; cin >> tamanioMatriz;
+
+        // Por si hay un error en el valor
+        valorCorrecto = true;
+    } while ( tamanioMatriz <= 0 || tamanioMatriz >= 101 || ( tamanioMatriz % 2 ) != 0 );
+
+    // Titulo para empezar con el llenado de la matriz
+    cout << " Ingresar la matriz de adyacencia " << endl << endl;
+
+    // Lectura de los valores
+    for ( int i = 0; i < tamanioMatriz; i++ )
+    {
+        for ( int k = 0; k < tamanioMatriz; k++ )
+        {
+            cout << endl << "[" << i <<"][" << k << "]: "; cin >> matriz[ i ][ k ];
+        }
+    }
+}
+
+
+// Revisar si la matriz tiene valores correctos
+bool matrizCorrecta( )
+{
+
+    // Comprobar si un vertice no tiene aristas
+    bool todoCeros = true;
+
+    // Comprobar si alguna arista tiene peso negativo
+    bool numeroNegativo = false;
+
+    // Recorrer toda la matriz
+    for ( int i = 0; i < tamanioMatriz; i++ )
+    {
+
+        // Reiniciar la variable en cada renglon
+        todoCeros = true;
+
+        // Recorrer las columnas
+        for ( int k = 0; k < tamanioMatriz; k++ )
+        {
+            // Regresar false si hay valor negativo
+            if ( matriz[ i ][ k ] < 0 ) numeroNegativo = true;
+
+            // Regresar false si hay todo ceros
+            if( matriz[ i ][ k ] > 0 ) todoCeros = false;
+        }
+
+        // No aceptar la matriz
+        if ( todoCeros || numeroNegativo )  return false;
+    }
+
+    return true;
 }
 
 
